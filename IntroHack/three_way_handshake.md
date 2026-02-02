c4a90f9186f0823e28d88b94c316f1fb43fc884a
OmarUTEC <javier.chavarria@utec.edu.pe>
2026-02-01 22:31:47 -0500

    New concept

---
+++ IntroHack/three_way_handshake.md
@@ -0,0 +1,4 @@
+TCP : Portocolo orientado a conexion -> Principio de Tthee-Way-Handshake
+UDP : Protocolo sin conexion
+
+
--- IntroHack/tuplas2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-mi_tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-
-numeros_impares = tuple(i for i in mi_tupla if i % 2 != 0)
-
-print(numeros_impares)
-
-
